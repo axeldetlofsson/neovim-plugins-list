44143db1face99664f07e475cef2f8c36782fa73
Axel Detlofsson <axeldetlof@gmail.com>
2022-08-19 00:10:10 +0200

    update list.json at 2022-08-19-00-10

--- fetch_and_push.fish
+++ fetch_and_push.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
-gh search repos --topic 'neovim-plugin' --limit 10 --json 'createdAt,defaultBranch,description,forksCount,fullName,hasDownloads,hasIssues,hasPages,hasProjects,hasWiki,homepage,id,isArchived,isDisabled,isFork,isPrivate,language,license,name,openIssuesCount,owner,pushedAt,size,stargazersCount,updatedAt,url,visibility,watchersCount' > list.json
+gh search repos --topic 'neovim-plugin' --limit 1500 --json 'createdAt,defaultBranch,description,forksCount,fullName,hasDownloads,hasIssues,hasPages,hasProjects,hasWiki,homepage,id,isArchived,isDisabled,isFork,isPrivate,language,license,name,openIssuesCount,owner,pushedAt,size,stargazersCount,updatedAt,url,visibility,watchersCount' > list.json
 prettier --write list.json
 git add .
-set -l current_date (date +%Y-%m-%d)
+set -l current_date (date +%Y-%m-%d-%H-%M)
 git commit -m "update list.json at $current_date"
 git push
 
